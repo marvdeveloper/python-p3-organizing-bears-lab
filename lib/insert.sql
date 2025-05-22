92e6bb08679b8b345dfda44cab831e7c9a80e0aa
Marvin Kadoyo <kadoyomarvin@gmail.com>
2025-05-22 17:21:09 +0300

    Insert initial bear records

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 8, 'M', 'Brown', 'Calm', 1),
+('Rowdy', 4, 'M', 'Black', 'Aggressive', 1),
+('Tabitha', 5, 'F', 'Golden', 'Sweet', 1),
+('Sergeant Brown', 9, 'M', 'Dark Brown', 'Grumpy', 1),
+('Melissa', 6, 'F', 'White', 'Friendly', 1),
+('Grinch', 7, 'M', 'Greenish', 'Mean', 0),
+('Wendy', 3, 'F', 'Gray', 'Happy', 1),
+(NULL, 2, 'M', 'Red', 'Unknown', 0);
